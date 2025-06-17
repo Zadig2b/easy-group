350454e23f202d03123abd59b2c693fd48f3a883
romain <romain.dugeay@gmail.com>
2025-06-17 14:10:52 +0200

    fixed test ?

--- base-back/src/test/java/com/base/SmartHouseBackendApplicationTests.java
+++ base-back/src/test/java/com/base/SmartHouseBackendApplicationTests.java
@@ -2,12 +2,13 @@ package com.base;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = BaseApp.class)
+@ActiveProfiles("test")
 class SmartHouseBackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
+
